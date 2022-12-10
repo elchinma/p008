054aa40c78081e3f9e5548364b99a73600c67f5f
elchinma <elchinma@code.edu.az>
2022-12-11 00:18:58 +0400

    p008

---
+++ ConsoleApp1/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class City
+    {
+        public string Name;
+        public float Population;
+        
+        public City(string name, float population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] arr = { 300000, 1000000 };
+
+            Console.WriteLine("Minimum number is " + arr.Min());
+            Console.WriteLine("Maximum number is " + arr.Max());
+        }
+    }
+}
+
+   
